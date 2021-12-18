9df140cdf4cb6c7227a7e9960f3008e8b3441d76
courtneymorris <shull.courtney@gmail.com>
2021-12-18 10:44:07 -0700

    Initial commit for heroku

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../style/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Here we go!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
