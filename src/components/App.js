1f07dd9040be41c3fd4a09c3fdec5c3bc0a1eec3
courtneymorris <shull.courtney@gmail.com>
2021-12-18 11:06:08 -0700

    Remote repo initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import "../style/main.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Here we go!</p>
-      </header>
-    </div>
-  );
-}
+import Home from "./home";
 
-export default App;
+import React, { Component } from "react";
+import { Router, Route } from "react-router-dom";
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app-wrapper">
+        <Router>
+          <Route path="/" component={Home} />
+        </Router>
+      </div>
+    );
+  }
+}
---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="home-wrapper">
+        <h1>Hello From the Home Page</h1>
+      </div>
+    );
+  }
+}
